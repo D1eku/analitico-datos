60c2b03ca3d6aae38f8e56211dcfdef71262784a
D1eku <45185542+D1eku@users.noreply.github.com>
2021-04-22 01:45:35 -0400

    Main.py Para ETL

---
+++ Tarea 1 ETL/main.py
@@ -0,0 +1,16 @@
+
+print("Hola Mundo")
+
+import numpy as pd
+import pandas as pd
+
+
+import os
+
+dataset = pd.read_csv("dataset1.csv")
+
+print(dataset)
+
+#Hacer el modelo 
+# y utilizar pandas para dividir las tablas 
+
